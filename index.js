1e69c69e476bf2e5ce404fa34f3a00e1ac0c58f6
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2023-01-16 13:08:40 -0800

    Added access control header

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(`${__dirname}/public`));
 
 app.post("/chatbot", (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*')
     const message = req.body.message;
     const number = message.match(/\d+/);
     if (number) {
