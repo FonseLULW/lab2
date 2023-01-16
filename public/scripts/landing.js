4e965ab4dfa541f8fe7d50549ba07cb597509341
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2023-01-16 13:07:16 -0800

    Changed URL to render.com URL

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		const message = $('#chatbot-input').val();
 		$.ajax({
 			type: 'POST',
-			url: 'http://localhost:3000/chatbot',
+			url: 'https://lab2-fpgd.onrender.com',
 			data: {
 				message: message
 			},
